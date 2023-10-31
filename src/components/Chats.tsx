09dd88fdfdaf2d1789512cb554464edbb5d7a629
Kevin <kjrodgers26@gmail.com>
2023-10-31 14:03:04 -0400

    Integrated a new chat sdk inplace of ChatEngine

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,67 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { ChatEngine } from "react-chat-engine";
+import SendbirdApp from "@sendbird/uikit-react/App";
+import "@sendbird/uikit-react/dist/index.css";
 import { auth } from "../firebase";
 import "../styles/Chats.css";
 
 import { useAuth } from "../contexts/AuthContext";
-import axios from "axios";
 
 const Chats: React.FC = () => {
-	const [loading, setLoading] = useState(true);
 	const navigate = useNavigate();
 	const user = useAuth();
-	const projectId = "f407ea32-1525-4c62-b59c-bf9ec90c985d";
+	//const projectId = "f407ea32-1525-4c62-b59c-bf9ec90c985d";
 
 	const handleLogout = async () => {
 		await auth.signOut();
 		navigate("/");
 	};
 
-	const getFile = async (url: string) => {
-		const response = await fetch(url);
-		const data = await response.blob();
-
-		return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
-	};
-
-	useEffect(() => {
-		if (!user) {
-			navigate("/");
-			return;
-		}
-
-		axios
-			.get("https://api.chatengine.io/users/me/", {
-				headers: {
-					"Project-ID": projectId,
-					"User-Name": user.email,
-					"User-Secret": user.uid,
-				},
-			})
-			.then(() => setLoading(false))
-			.catch(() => {
-				const formdata = new FormData();
-				formdata.append("email", user.email!);
-				formdata.append("username", user.email!);
-				formdata.append("secret", user.uid);
-
-				getFile(user.photoURL as string).then((avatar) => {
-					formdata.append("avatar", avatar, avatar.name);
-
-					axios
-						.post("https://api.chatengine.io/users/", formdata, {
-							headers: {
-								"private-key": "7f6f4c76-e2f3-4d2f-af34-c67147ae74a0",
-							},
-						})
-						.then(() => setLoading(false))
-						.catch((error) => console.log(error));
-				});
-			});
-	}, [user, navigate]);
-
-	if (!user || loading) return "Loading...";
+	if (!user) return "Loading...";
 
 	return (
 		<div className="chats-page">
@@ -71,13 +27,15 @@ const Chats: React.FC = () => {
 					Logout
 				</div>
 			</div>
-
-			<ChatEngine
-				height="calc(100vh - 66px)"
-				projectID={projectId}
-				userName={user.email}
-				userSecret={user.uid}
-			/>
+			<div style={{ height: "100vh" }}>
+				<SendbirdApp
+					appId="23351325-763E-4991-846F-9361AA4687B8"
+					userId={user.email!}
+					theme="dark"
+					nickname={user.displayName!}
+					profileUrl={user.photoURL!}
+				/>
+			</div>
 		</div>
 	);
 };
